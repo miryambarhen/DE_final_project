44c235235994023bccaa56e518e5448e2e2f8563
Miryam Bar Hen <miryambar98@gmail.com>
2023-03-09 18:46:51 +0200

    adding a data frame to mongodb tables

--- mongodb/articles.py
+++ mongodb/articles.py
@@ -1,9 +1,16 @@
+import pandas as pd
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
 db = client["stocks_db"]
 col = db["articles"]
 
-# show the data
-for x in col.find():
-    print(x)
+# Fetch data from MongoDB
+results = list(col.find())
+
+# Convert data to DataFrame
+df = pd.DataFrame(results, columns=["date", "ticker", "published_at", "title", "publisher", "author", "article"])
+# Set display option to show all columns
+pd.set_option("display.max_columns", None)
+
+print(df)
